1ea763e277d4435d34323aebf4c613a5f52fc171
gustavoalissont2t <gustavo.alissont2t@gmail.com>
2021-06-23 02:19:51 -0300

    Atualização do seletor

--- selectors.py
+++ selectors.py
@@ -3,7 +3,7 @@ selectors = {
         "COOKIES": '//*[@id="main-wrapper"]/header/div[1]/div/div/ul/li[1]/button'
     },
     "LINK": {
-        "NOTICE": 'highlight__link'
+        "NOTICE": 'highlight__link > a'
     }
 
 }
